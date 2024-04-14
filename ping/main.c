3aa75f097698040553586bec422f4c9531de4d97
Benjamin Brassart <bbrassar@student.42.fr>
2024-04-14 17:18:06 +0200

    Add main loop with signal handling

--- ping/main.c
+++ ping/main.c
@@ -6,12 +6,13 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 15:03:37 by bbrassar          #+#    #+#             */
-/*   Updated: 2024/04/14 16:20:24 by bbrassar         ###   ########.fr       */
+/*   Updated: 2024/04/14 17:17:28 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ping.h"
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -55,10 +56,54 @@ int main(int argc, char const *argv[])
 	return result == -1 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
-static int ft_ping(struct ft_ping const *ping, int fd)
+static volatile sig_atomic_t _SEND = 1;
+static volatile sig_atomic_t _RUN = 1;
+
+static void _handle_sigint(int sig)
+{
+	(void)sig;
+	_RUN = 0;
+}
+
+static void _handle_sigalrm(int sig)
+{
+	(void)sig;
+	_SEND = 1;
+}
+
+static void _send_ping_packet(struct ft_ping const *ping, int fd)
 {
 	(void)ping;
 	(void)fd;
-	// TODO implement ft_ping core function
+}
+
+static void _receive_ping_packet(struct ft_ping const *ping, int fd)
+{
+	(void)ping;
+	(void)fd;
+}
+
+static int ft_ping(struct ft_ping const *ping, int fd)
+{
+	printf("PING %s (%s): %d data bytes", ping->host, ping->saddr, 56);
+	if (ping->flag_verbose) {
+		printf(", id 0x%1$04hx = %1$hu", (uint16_t)getpid());
+	}
+	printf("\n");
+
+	signal(SIGINT, _handle_sigint);
+
+	while (_RUN)
+	{
+		if (_SEND) {
+			_SEND = 0;
+			alarm(1U);
+			signal(SIGALRM, _handle_sigalrm);
+			_send_ping_packet(ping, fd);
+		}
+		_receive_ping_packet(ping, fd);
+	}
+	alarm(0U);
+
 	return 0;
 }
