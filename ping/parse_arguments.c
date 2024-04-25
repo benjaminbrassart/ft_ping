e7cb61cd14b0f79e946e458b4efb097c99585b51
Benjamin Brassart <bbrassar@student.42.fr>
2024-04-25 21:50:19 +0200

    Add option delimiter

--- ping/parse_arguments.c
+++ ping/parse_arguments.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 15:17:59 by bbrassar          #+#    #+#             */
-/*   Updated: 2024/04/20 16:56:27 by bbrassar         ###   ########.fr       */
+/*   Updated: 2024/04/25 21:50:16 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 
 int parse_arguments(struct ft_ping *ping, int argc, char const *argv[])
 {
+	unsigned delimiter = 0;
+
 	ping->flags.quiet = 0;
 	ping->flags.debug = 0;
 	ping->flags.verbose = 0;
@@ -29,8 +31,10 @@ int parse_arguments(struct ft_ping *ping, int argc, char const *argv[])
 	ping->host = NULL;
 
 	for (int i = 1; i < argc; i += 1) {
-		if (argv[i][0] == '-' && argv[i][1] != '\0') {
-			if (strcmp("-?", argv[i]) == 0) {
+		if (!delimiter && argv[i][0] == '-' && argv[i][1] != '\0') {
+			if (strcmp("--", argv[i]) == 0) {
+				delimiter = 1;
+			} else if (strcmp("-?", argv[i]) == 0) {
 				ping->flags.help = 1;
 				return 0;
 			} else if (strcmp("-v", argv[i]) == 0) {
